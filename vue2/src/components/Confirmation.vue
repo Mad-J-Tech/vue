65b17dc2e2c437534a37ea3c288cc6d068eb9ee7
JIn Kimura <jellyfishman520@gmail.com>
2021-12-01 21:15:52 +0900

    [update]Vue②-２Confirmation.vue state参照方法の修正

--- vue2/src/components/Confirmation.vue
+++ vue2/src/components/Confirmation.vue
@@ -4,35 +4,35 @@
       <b-card header="以下の内容をご確認ください" header-class="text-center">
         <div>
           <p class="text-info">-性別-</p>
-          <p>{{ this.$store.state.gender }}</p>
+          <p>{{ this.$store.getters.gender }}</p>
         </div>
         <div>
           <p class="text-info">-生年月日-</p>
           <p>
-            {{ this.$store.state.birthYear }}年{{
-              this.$store.state.birthMonth
-            }}月{{ this.$store.state.birthDate }}日
+            {{ this.$store.getters.birthYear }}年{{
+              this.$store.getters.birthMonth
+            }}月{{ this.$store.getters.birthDate }}日
           </p>
         </div>
         <div>
           <p class="text-info">-現在、生命保険に加入されていますか？-</p>
-          <p>{{ this.$store.state.insurance }}</p>
+          <p>{{ this.$store.getters.insurance }}</p>
         </div>
         <div>
           <p class="text-info">
             -現在入院中ですか。または、最近３ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？-
           </p>
-          <p>{{ this.$store.state.hospitalization }}</p>
+          <p>{{ this.$store.getters.hospitalization }}</p>
         </div>
         <div>
           <p class="text-info">
             -過去５年以内に、病気や怪我で、手術をうけたことまたは継続して７日以上の入院をしたことがありますか？-
           </p>
-          <p>{{ this.$store.state.surgery }}</p>
+          <p>{{ this.$store.getters.surgery }}</p>
         </div>
         <div>
           <p class="text-info">-ご相談内容-</p>
-          <p>{{ this.$store.state.text }}</p>
+          <p>{{ this.$store.getters.text }}</p>
         </div>
       </b-card>
     </b-card-group>
