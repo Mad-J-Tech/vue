0c15533c9505b45ce9bc013ce78e8fc013502686
JIn Kimura <jellyfishman520@gmail.com>
2022-01-23 10:54:22 +0900

    vue③ [update]todoリスト配列の統合その他表示修正

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -21,48 +21,8 @@
               <b-th>削除</b-th>
             </b-tr>
           </b-thead>
-          <b-tbody v-if="selectedAll">
+          <b-tbody>
             <b-tr v-for="item in getTodos" v-bind:key="item.id">
-              <b-th>{{ item.id }}</b-th>
-              <b-td>{{ item.comment }}</b-td>
-              <b-td
-                ><b-button @click="changeStatus(item)">{{
-                  item.status ? "完了" : "作業中"
-                }}</b-button></b-td
-              >
-              <b-td>
-                <b-button
-                  @click="
-                    doRemove(item);
-                    reNumber();
-                  "
-                  >削除</b-button
-                >
-              </b-td>
-            </b-tr>
-          </b-tbody>
-          <b-tbody v-if="selectedStill">
-            <b-tr v-for="item in getCurrentTodos" v-bind:key="item.id">
-              <b-th>{{ item.id }}</b-th>
-              <b-td>{{ item.comment }}</b-td>
-              <b-td
-                ><b-button @click="changeStatus(item)">{{
-                  item.status ? "完了" : "作業中"
-                }}</b-button></b-td
-              >
-              <b-td>
-                <b-button
-                  @click="
-                    doRemove(item);
-                    reNumber();
-                  "
-                  >削除</b-button
-                >
-              </b-td>
-            </b-tr>
-          </b-tbody>
-          <b-tbody v-if="selectedDone">
-            <b-tr v-for="item in getDoneTodos" v-bind:key="item.id">
               <b-th>{{ item.id }}</b-th>
               <b-td>{{ item.comment }}</b-td>
               <b-td
@@ -93,12 +53,10 @@
     </b-card-group>
   </b-container>
 </template>
-
 <script>
 export default {
   data() {
     return {
-      selected: "all",
       options: [
         { text: "全て", value: "all" },
         { text: "作業中", value: "still" },
@@ -110,47 +68,34 @@ export default {
     getTodos() {
       return this.$store.getters.todos;
     },
-    getDoneTodos() {
-      return this.$store.getters.doneTodos;
-    },
-    getCurrentTodos() {
-      return this.$store.getters.currentTodos;
-    },
-    selectedAll() {
-      return this.selected == "all";
-    },
-    selectedStill() {
-      return this.selected == "still";
-    },
-    selectedDone() {
-      return this.selected == "done";
+    selected: {
+      get() {
+        return this.$store.state.selected;
+      },
+      set(val) {
+        this.$store.commit("setStatus", val);
+      },
     },
   },
   methods: {
     doAdd() {
-      let comment = this.$refs.comment;
+      const comment = this.$refs.comment;
       if (!comment.value.length) {
         return;
       }
-
       this.$store.commit("doAdd", comment.value);
-
       comment.value = "";
     },
     doRemove(item) {
-      let index = this.getTodos.indexOf(item);
-
+      const index = this.getTodos.indexOf(item);
       this.$store.commit("doRemove", index);
     },
     reNumber() {
       this.$store.commit("reNumber");
     },
     changeStatus(item) {
-      let index = this.getTodos.indexOf(item);
+      const index = this.getTodos.indexOf(item);
       this.$store.commit("changeStatus", index);
-    },
-    select() {
-      console.log("selected");
     },
   },
 };
--- vue3/src/store/index.js
+++ vue3/src/store/index.js
@@ -1,42 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from "vuex-persistedstate";
-
 Vue.use(Vuex);
-
-
-
 const store = new Vuex.Store({
-
     plugins: [createPersistedState({ storage: window.sessionStorage })],
-
     state: {
-        todos: [
-
-        ]
+        todos: [],
+        selected: "all"
     },
     getters: {
-        todos: state => state.todos,
-        doneTodos: state => {
-            return state.todos.filter(todo => todo.status)
+        todos: state => {
+            if (state.selected === "all") {
+                return state.todos;
+            } else if (state.selected === "still") {
+                return state.todos.filter(todo => !todo.status)
+            } else if (state.selected === "done") {
+                return state.todos.filter(todo => todo.status)
+            }
         },
-        currentTodos: state => {
-            return state.todos.filter(todo => !todo.status)
-        },
-
     },
     mutations: {
         doAdd(state, comment) {
             state.todos.push({
                 id: state.todos.length,
-                comment: comment,
+                comment,
                 status: Boolean(false),
             })
         },
         doRemove(state, index) {
             state.todos.splice(index, 1)
         },
-
         reNumber(state) {
             state.todos.forEach(function (el, index) {
                 state.todos[index].id = index;
@@ -45,9 +38,9 @@ const store = new Vuex.Store({
         changeStatus(state, index) {
             state.todos[index].status = !state.todos[index].status;
         },
-
-
+        setStatus(state, val) {
+            state.selected = val;
+        }
     }
 })
-
 export default store
